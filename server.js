ebb8b5ba556ba82421fe03f468ff7ee60b3dbb47
Stefan Rotaru <stefanrotaru@hotmail.com>
2022-03-10 04:03:12 +0200

    second commit

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.listen(3001, () => {
-	console.log('app is running on port 3001')
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`app is running on port ${process.env.PORT}`)
 })
 
 
