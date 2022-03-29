e7b740ade16c734fd80b27a0b75ada41b17542a7
Stefan Rotaru <stefanrotaru@hotmail.com>
2022-03-29 12:22:43 +0300

    fixed indentation

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'WTHamDoing',
-    database : 'smart-brain'
-  }
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "WTHamDoing",
+    database: "smart-brain",
+  },
 });
 
 const app = express();
@@ -23,20 +23,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send('success')
-})
+app.get("/", (req, res) => {
+  res.send("success");
+});
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
 
 app.listen(process.env.PORT || 3001, () => {
-	console.log(`app is running on port ${process.env.PORT}`)
-})
-
-
+  console.log(`app is running on port ${process.env.PORT}`);
+});
 
 /* !!PLAN!!
 
